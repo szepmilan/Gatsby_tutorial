3c5982038a21c054a9a36730d92564e6b12a06cf
szepmilan <112125031+szepmilan@users.noreply.github.com>
2022-10-02 20:52:17 +0200

    46. siteMetaData

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,26 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata:{
+    title:'Simply Recipes',
+    description:"Nice and clean recipes site",
+    author:"@szepmilu",
+    person:{
+      name:"milan",
+      age:23
+    },
+    simpleData: ["item 1", "item 2"],
+    complexData: [
+      {
+        name:"milan",
+        age:23
+      },
+      {
+        name:"tina",
+        age:20
+      },
+    ]
+  },
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../components/Layout"
 import { StaticImage } from "gatsby-plugin-image"
+//import SEO from "../components/SEO"
 
 export default function Home() {
   return (
