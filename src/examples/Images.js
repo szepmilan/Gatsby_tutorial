dec4fc840b1c21ad80032343463e86d527965f99
szepmilan <112125031+szepmilan@users.noreply.github.com>
2022-10-02 18:54:11 +0200

    40. Height

--- src/examples/Images.js
+++ src/examples/Images.js
@@ -7,7 +7,7 @@ const Images = () => {
   return (
     <Wrapper>
       <article>
-        <h4>constrained / default</h4>
+        <h4>constrained</h4>
         <StaticImage
           src='../assets/images/recipe-1.jpeg' alt="food"
           placeholder="tracedSVG"
@@ -23,7 +23,6 @@ const Images = () => {
           placeholder="blurred"
           layout='fixed'
           width={200}
-          height={600}
           className='example-img'
           as='div'
         />
@@ -53,10 +52,14 @@ const Wrapper = styled.section`
   }
   .example-img{
     border-radius: 1rem;
-  }/*
+    height: 300px;
+  }
   @media (min-width:992px){
     grid-template-columns:1fr 1fr 1fr;
-  }*/
+    .example-img{
+      height: 200px;
+    }
+  }
 `
 
 export default Images
