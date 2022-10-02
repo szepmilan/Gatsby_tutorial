6232e31066686baf1237acab56f1fe2c641f0653
szepmilan <112125031+szepmilan@users.noreply.github.com>
2022-10-02 19:10:02 +0200

    41. About Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from "react"
 import Layout from "../components/Layout"
-import Images from "../examples/Images"
 
 export default function Home() {
   return (
     <Layout>
       <h1>home page</h1>
-      <Images/>
     </Layout>
   )
 }
