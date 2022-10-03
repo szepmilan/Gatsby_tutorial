1c7d5d51a5d21e6675c901d7946331a45624578e
szepmilan <112125031+szepmilan@users.noreply.github.com>
2022-10-03 22:20:58 +0200

    72. ENV Variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,9 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
   /* Your site config here */
@@ -36,6 +39,14 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/assets/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `6fmmh1lxi7a0`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_API_KEY,
       },
     },
   ],
