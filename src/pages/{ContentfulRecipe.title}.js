14127577745f73232cad6114675f6f6a87537175
szepmilan <112125031+szepmilan@users.noreply.github.com>
2022-10-04 02:48:21 +0200

    85. Recipe Template Query

--- src/pages/{ContentfulRecipe.title}.js
+++ src/pages/{ContentfulRecipe.title}.js
@@ -1,17 +1,39 @@
 import React from 'react'
 import { graphql } from 'gatsby';
-const RecipeTemplate = props => {
-  console.log(props);
+import { GatsbyImage,getImage } from 'gatsby-plugin-image';
+import {BsClockHistory, BsClock, BsPeople} from 'react-icons/bs'
+const RecipeTemplate = ({data}) => {
+  const {title,cookTime,content, prepTime, servings,description:{description},image,} = data.contentfulRecipe;
+  const pathToImage =getImage(image);
+  const {tags,instructions,ingredients,tools} = content;
   return (
-    <h2>{props.params.title}</h2>
+    <main className='page'>
+      <div className='recipe-page'>
+        <h2>{title}</h2>
+      </div>
+    </main>
   )
 }
 
 export const query =graphql`
   query getSingleRecipe($title: String) {
-      contentfulRecipe(title: {eq: $title}) {
+    contentfulRecipe(title: {eq: $title}) {
       title
       cookTime
+      content {
+        ingredients
+        instructions
+        tags
+        tools
+      }
+      description {
+        description
+      }
+      prepTime
+      servings
+      image {
+        gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+      }
     }
   }
 `
