45dab491964a71c195c92150ea6c3cc51f3fa40f
szepmilan <112125031+szepmilan@users.noreply.github.com>
2022-10-04 02:39:12 +0200

    84. Query Variables

--- src/pages/{ContentfulRecipe.title}.js
+++ src/pages/{ContentfulRecipe.title}.js
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import { graphql } from 'gatsby';
 const RecipeTemplate = props => {
   console.log(props);
   return (
     <h2>{props.params.title}</h2>
   )
 }
+
+export const query =graphql`
+  query getSingleRecipe($title: String) {
+      contentfulRecipe(title: {eq: $title}) {
+      title
+      cookTime
+    }
+  }
+`
 
 export default RecipeTemplate
