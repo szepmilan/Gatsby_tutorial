6e6eeb25d61260c0301b2215572f110a7db85931
szepmilan <112125031+szepmilan@users.noreply.github.com>
2022-10-05 02:55:48 +0200

    94. SEO - Setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,5 +66,6 @@ module.exports = {
         },
       },
     },
+    `gatsby-plugin-react-helmet`,
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,13 @@ import React from "react"
 import Layout from "../components/Layout"
 import { StaticImage } from "gatsby-plugin-image"
 import AllRecipes from "../components/AllRecipes"
+import Seo from "../components/SEO"
 //import FetchData from '../examples/fetchData'
-//import SEO from "../components/SEO"
 
 export default function Home() {
   return (
     <Layout>
+      <Seo/>
       <main className="page">
         <header className="hero">
           <StaticImage 
